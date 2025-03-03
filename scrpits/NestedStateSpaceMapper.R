e5e4e0a68b6703a5d857b159c5105d1f458f7a81
Ed-VanDerSar <edwin.anzu@gmail.com>
2025-03-03 12:02:55 -0600

    Update NestedStateSpaceMapper.R

--- scrpits/NestedStateSpaceMapper.R
+++ scrpits/NestedStateSpaceMapper.R
@@ -26,7 +26,7 @@ state_space_mapper <- function(n) {
 }
 
 #' Given integers n and b, returna a matrix encoding
-#' all possible states of the nestes Kingman model
+#' all possible states of the nested Kingman model
 #' starting with s species with n genes each. Each row of
 #' the matrix represents a sample where the i-th coordinate
 #' is the number speciec with i genes.
@@ -35,19 +35,28 @@ state_space_mapper <- function(n) {
 #' @param b the size of the initial species sample.
 nested_state_space_mapper <- function(n, b) {
   valid_vectors <- list()
-  for (k in 1:n * b) {
+  for (k in 1:(n * b)) {
     a <- state_space_mapper(k)
-    zero_cols <- matrix(0, nrow = NROW(a), ncol =   b * n - k)
+    zero_cols <- matrix(0, nrow = NROW(a), ncol =   (b * n) - k)
     a_new <- cbind(a, zero_cols)
     valid_vectors <- append(valid_vectors, split(a_new, row(a_new)))
   }
+  valid_vectors <- matrix(
+                          unlist(valid_vectors),
+                          nrow = length(valid_vectors),
+                          byrow = TRUE)
   return(valid_vectors)
 }
 
-cardinality_state_space(n, b) {
-  e = 0
-   for (k in 1:n * b) {
-    e <- P()
+#' Given integers n and b, returna the cardinality of the stae space
+#' of  of the nested Kingman model starting with s species with n genes each.
+#'
+#' @param n the size of the initial gene sample.
+#' @param b the size of the initial species sample.
+cardinality_state_space <- function(n, b) {
+  e <- 0
+  for (k in 1:(n * b)) {
+    e <- e + P(k)
   }
-  return(valid_vectors)
+  return(e)
 }
