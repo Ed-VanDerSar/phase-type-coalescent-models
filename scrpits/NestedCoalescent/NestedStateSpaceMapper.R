f04dad276c7d3946698bd94faac0892027930866
Ed-VanDerSar <edwin.anzu@gmail.com>
2025-04-30 15:11:00 -0600

    Update NestedStateSpaceMapper.R

--- scrpits/NestedCoalescent/NestedStateSpaceMapper.R
+++ scrpits/NestedCoalescent/NestedStateSpaceMapper.R
@@ -27,27 +27,44 @@ state_space_mapper <- function(n) {
 }
 
 #' Given integers n and b, returna a matrix encoding
-#' all possible states of the nested Kingman model
+#' all possible truncated_ of the nested Kingman model
 #' starting with s species with n genes each. Each row of
 #' the matrix represents a sample where the i-th coordinate
-#' is the number speciec with i genes.
+#' is the number species with i genes.
 #'
-#' @param n the size of the initial gene sample.
+#' @param n the size of the initial gene sample in each species.
 #' @param b the size of the initial species sample.
 #' @return the state space.
 nested_state_space_mapper <- function(n, b) {
-  valid_vectors <- list()
-  for (k in 1:(n * b)) {
-    a <- state_space_mapper(k)
-    zero_cols <- matrix(0, nrow = NROW(a), ncol =   (b * n) - k)
-    a_new <- cbind(a, zero_cols)
-    valid_vectors <- append(valid_vectors, split(a_new, row(a_new)))
+  valid_states <- list()
+  # Process and add all remaining states by its gene mass
+  for (k in 1:((n * b) - 1)) {
+    truncated_states <- state_space_mapper(k)
+    zero_cols <- matrix(0, nrow = NROW(truncated_states), ncol =   (b * n) - k)
+    states <- cbind(truncated_states, zero_cols)
+    valid_states <- append(valid_states, split(states, row(states)))
   }
-  valid_vectors <- matrix(
-                          unlist(valid_vectors),
-                          nrow = length(valid_vectors),
-                          byrow = TRUE)
-  return(valid_vectors)
+  ## Reorder to choose the first state as the one with b species with n genes
+  initial_states <- state_space_mapper((n * b))
+  # Find the index of the row with the maximum value in column i
+  max_index <- which.max(initial_states[, n])
+  # Reorder the matrix: put all rows except max_index first,
+  # then add max_index row at the end
+  reordered_initial_states <- rbind(
+                                    initial_states[-max_index, , drop = FALSE],
+                                    initial_states[max_index, , drop = FALSE])
+  valid_states <- append(
+                         valid_states,
+                         split(
+                               reordered_initial_states,
+                               row(reordered_initial_states)))
+  valid_states <- matrix(
+                         unlist(valid_states),
+                         nrow = length(valid_states),
+                         byrow = TRUE)
+  # reorder states
+  ordered_valid_states <-  valid_states[rev(seq_len(nrow(valid_states))), ]
+  return(ordered_valid_states)
 }
 
 rate_matrix <- function(e) {
@@ -56,12 +73,12 @@ rate_matrix <- function(e) {
   rate <- matrix(0, ncol = dim, nrow = dim)
   for (i in 2:dim) {
     for (j in 1:(i - 1)) {
-      ## establishing differences between two states
+      ## establishing differences between two truncated_
       c <- e[i, ] - e[j, ]
-      ## Identifyung if the two states are compatible
+      ## Identifying if the two truncated_ are compatible
       sum1 <- c %*% rep(1:total_gene_sample)
       gene_mass <- sum1[1, 1]
-      ## check1==0 means that the size of the new blocks 
+      ## check1==0 means that the size of the new blocks
       ## equals the size of the disappearing blocks
       ## Identifying how many new blocks are created
       w1 <- ifelse(c > 0, 1, 0)
